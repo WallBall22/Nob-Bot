c5587f2c0902a3dd89f9aeb59cf62f329a30bbaa
chadi1fekkari <40345231+chadi1fekkari@users.noreply.github.com>
2018-12-09 09:31:20 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -89,7 +89,45 @@ client.user.setGame(`بدايه تحت التطوير`,"https://www.twitch.tv/dg
     channel.send({embed : embed});
 
     });
+ client.on('message', message => {
+
+  if (true) {
+
+if (message.content === '.invite') {
+
+      message.author.send('  https://discordapp.com/api/oauth2/authorize?client_id=521253056218791947&permissions=8&scope=bot |  تفضل ربط البوت     ').catch(e => console.log(e.stack));
+
  
+
+    }
+
+   }
+
+  });
+client.on('message', message => {
+
+     if (message.content === ".invite") {
+
+     let embed = new Discord.RichEmbed()
+
+  .setAuthor(message.author.username)
+
+  .setColor("#9B59B6")
+
+  .addField(" Done | تــــم" , " |  تــــم ارســالك في الخــاص")
+
+     
+
+     
+
+     
+
+  message.channel.sendEmbed(embed);
+
+    }
+
+});
+
 
 
 client.login(process.env.BOT_TOKEN);
