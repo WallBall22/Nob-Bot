5aebe25e0439484107221be216c21377b1b70822
chadi1fekkari <40345231+chadi1fekkari@users.noreply.github.com>
2018-12-09 08:58:05 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -29,7 +29,67 @@ client.user.setGame(`بدايه تحت التطوير`,"https://www.twitch.tv/dg
   console.log('')
   console.log('')
 });
+ client.on('message', message => {
+    var prefix = "*";
+    
+        if (message.author.id === client.user.id) return;
+        if (message.guild) {
+       let embed = new Discord.RichEmbed()
+        let args = message.content.split(' ').slice(1).join(' ');
+    if(message.content.split(' ')[0] == prefix + 'bc') {
+        if (!args[1]) {
+    message.channel.send("**f!bc <message>**");
+    return;
+    }
+            message.guild.members.forEach(m => {
+       if(!message.member.hasPermission('ADMINISTRATOR')) return;
+                var bc = new Discord.RichEmbed()
+                .addField('» السيرفر :', `${message.guild.name}`)
+                .addField('» المرسل : ', `${message.author.username}#${message.author.discriminator}`)
+                .addField(' » الرسالة : ', args)
+                .setColor('#ff0000')
+                // m.send(`[${m}]`);
+                m.send(`${m}`,{embed: bc});
+            });
+        }
+        } else {
+            return;
+        }
+    });
+ client.on('guildMemberAdd', msg => { 
+
+    var embed = new Discord.RichEmbed()
+
+    .setAuthor(msg.user.username, msg.user.avatarURL)
+
+    .setThumbnail(msg.user.avatarURL)
+
+    .setImage('https://cdn.pg.sa/4J1jipROqB.jpg')     
+
+    .setTitle('New Member!')
+
+    .setDescription('Welcome To server')
+
+    .addField('**ID Member:',"" +  msg.user.id, true)
+
+    .addField('**Tag Member**', msg.user.discriminator, true)
+
+    .addField('**Member Created At', msg.user.createdAt, true)
+
+    .addField(' 👤   You Number',`**[ ${msg.guild.memberCount} ]**`,true)
+
+    .setColor('GREEN')
+
+    .setFooter(msg.guild.name, msg.guild.iconURL, true)
+
+    var channel = msg.guild.channels.find('name', 'chat')         
+
+    if (!channel) return;
+
+    channel.send({embed : embed});
+
+    });
  
- 
- 
+
+
 client.login(process.env.BOT_TOKEN);
