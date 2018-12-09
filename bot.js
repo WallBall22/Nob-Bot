0e79afb4fcb2952697844cc5a7cd0b50cfe4aac6
chadi1fekkari <40345231+chadi1fekkari@users.noreply.github.com>
2018-12-09 20:09:28 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ const prefix = '*'
  
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`*invite`,"https://www.twitch.tv/dggamingbot")
+client.user.setGame(`*invite `,"https://www.twitch.tv/dggamingbot")
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
